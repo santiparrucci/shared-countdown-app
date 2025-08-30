858508ac15ab99812a9a38dcfe25e513fda627d8
Santiago <santiparrucci@gmail.com>
2025-08-30 00:37:41 -0300

    conexión bd y set up basico del back

---
+++ backend/config/config.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: { ssl: false },
+    logging: false,
+  },
+  test: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    logging: false,
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+    logging: false,
+  },
+};
